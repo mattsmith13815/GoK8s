f9818ee910cf7901f5f212c54a1ed99e952314f7
Matthew Smith <mattsmith13815@me.com>
2024-09-08 19:56:49 -0400

    updates

--- build_and_deploy.ps1
+++ build_and_deploy.ps1
@@ -1,13 +1,16 @@
 #Write-Output "Building Go app..."
-$Env:GOOS = "linux"
-$Env:GOARCH = "amd64"
-go build -o ./dist/
+#$Env:GOOS = "linux"
+#$Env:GOARCH = "amd64"
+#go build -o ./dist/
 
 #Write-Output "Building Docker image..."
-docker build -t localhost/gocontainer:latest .
+#docker build -t localhost/gocontainer:latest .
 
 #Write-Output "Pushing Docker image to local registry..."
 #docker push localhost/gocontainer:latest
+
+Write-Output "Pulling remote image..."
+docker pull ghcr.io/mattsmith13815/gok8s:main
 
 Write-Output "Remove current deployment if it exists..."
 kubectl delete deployment gocontainer-deployment
