f1ccd45f54da292e4d9de2cfe23d6d43e6c6a209
Matthew Smith <mattsmith13815@me.com>
2024-09-13 17:45:01 -0400

    Update Java

--- java-tomcat-app/src/main/java/com/example/HelloServlet.java
+++ java-tomcat-app/src/main/java/com/example/HelloServlet.java
@@ -9,6 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, World Java!</h1>");
+        response.getWriter().println("<h1>Hello, World  2!</h1>");
     }
 }
--- readme.md
+++ readme.md
@@ -47,6 +47,7 @@ kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 
 kubectl get deployments
+
 kubectl get pods
 kubectl get services
 
